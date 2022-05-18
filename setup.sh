403e07ace0693832531cd029cc12a7c5243b3ca1
Ajay <aj.verde79@gmail.com>
2022-05-18 18:57:25 -0500

    COMOON

--- setup.sh
+++ setup.sh
@@ -8,6 +8,6 @@ cp ssh_config /root/.ssh/config
 mv deploy_key /root/.ssh/deploy_key
 chmod 600 /root/.ssh/deploy_key
 
-ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
+ssh-keyscan -t rsa github.com >> /root/.ssh/known_hosts
 
 git clone git@github.com:Regen8/trying_autograde_plz /autograder/autograder-code
