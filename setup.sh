e26fa549a26fa22a577dd03bd2a7d57e5df80814
Ajay <aj.verde79@gmail.com>
2022-05-18 18:03:35 -0500

    updated keys and github.com host

--- setup.sh
+++ setup.sh
@@ -8,9 +8,6 @@ cp ssh_config /root/.ssh/config
 mv deploy_key /root/.ssh/deploy_key
 chmod 600 /root/.ssh/deploy_key
 
-ssh-keyscan -t rsa github.tamu.edu >> ~/.ssh/known_hosts
+ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
 
-ls -la /dev/tty
-ssh -vT git@github.tamu.edu
-
-git clone git@github.tamu.edu:Regen8/sample_gradescope /autograder/autograder-code
+git clone git@github.com:Regen8/trying_autograde_plz /autograder/autograder-code
