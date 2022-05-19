df8c21c89aae846594cfdb141a46c7eb85d6b90c
Ajay <aj.verde79@gmail.com>
2022-05-18 19:03:35 -0500

    I NEED HELP

--- setup.sh
+++ setup.sh
@@ -4,10 +4,11 @@ cd /autograder/source
 
 mkdir -p /root/.ssh
 cp ssh_config /root/.ssh/config
-
+# Make sure to include your private key here
 mv deploy_key /root/.ssh/deploy_key
 chmod 600 /root/.ssh/deploy_key
+# To prevent host key verification errors at runtime
+ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
 
-ssh-keyscan -t rsa github.com >> /root/.ssh/known_hosts
-
-git clone git@github.com:Regen8/trying_autograde_plz /autograder/autograder-code
+# Clone autograder files
+git clone git@github.com:Regen8/trying_autograde_plz.git /autograder/autograder-code
